946c0940fe26b19bcfd8b16b92793f3b8abe2f53
yaaseenk <yaaseenkhan02@gmail.com>
2025-10-09 21:57:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+const characterCodes = Array.from(Array(26)).map( (_,i) => i + 97);
+
+let passwordOptions = { 
+  numbers: [0,1,2,3,4,5,6,7,8,9],
+  symbols: ['!', '@', '#', '$', '^', '&', '*', '”' , '%', "'", '(', ')' , '+', '-' , '.', '/', ':', ';' , '<' , '=' , '>' , '?' , '~' ],
+
+  lowerCaseLetters: characterCodes.map(code => String.fromCharCode(code)),
+  upperCaseLetters: characterCodes.map(code => String.fromCharCode(code).toUpperCase())
+}
+console.log(passwordOptions.lowerCaseLetters[1])
+console.log(passwordOptions.upperCaseLetters[1])
+// numbers array 
+
+// symbols array
+
+// create an array of 26 elements and populate it with ASCII codes for the lowercase chracters
+// let characterCodes = Array.from(Array(26)).map( (_,i) => i + 97);
+// convert the ASCII codes to actual letters
+let lowerCaseLetters = characterCodes.map(code => String.fromCharCode(code));
+// convert lowercase letters to uppercase letters
+let upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
+
+function generatePassword() {
+  window.confirm('Do you want letters in your password?')
+  
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function getRandomInt(min, max) {
+  // return result to which ever function calls getRandomInt function;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(getRandomInt(10, 11))
